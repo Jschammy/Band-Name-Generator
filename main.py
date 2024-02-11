fe1689beeafcf2f06f150163ef824d70fd8ea22c
Justin Schamel <schamel.an.justin@gmail.com>
2024-02-11 12:05:41 -0500

    made print and input statements on lines 1-4.

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!\n")
+homeTown = input("What is your home town?\n")
+sigOther = input("Who is your significant other?\n")
+print("Your band name should be " + homeTown + " " + sigOther + ".")
